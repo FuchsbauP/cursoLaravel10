b0bce5f01a5b84409cb8798dda346d3e9e0632c9
FuchsbauP <fabio.souza160@hotmail.com>
2024-12-06 12:09:50 -0300

    feat: alert

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,17 +1,7 @@
-<h1 class="mb-4 text-2xl font-bold text-gray-800">Dúvida {{ $support->id }}</h1>
+<h1>Dúvida {{ $support->id }}</h1>
 
-<!-- Mensagens de Erro -->
-@if ($errors->any())
-    <div class="mb-4 rounded bg-red-100 p-4 text-red-700">
-        <ul class="list-disc pl-5">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+<x-alert />
 
-<!-- Formulário -->
 <form action="{{ route('supports.update', $support->id) }}" method="POST" class="rounded bg-white p-6 shadow-md">
     @method('PUT')
     @include('admin.supports.partials.form', [
